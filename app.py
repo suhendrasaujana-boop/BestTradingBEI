3a21818c3ddcd6f1b5401eb2ca6b9c59b56b34d9
suhendrasaujana-boop <272155887+suhendrasaujana-boop@users.noreply.github.com>
2026-04-17 12:53:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
 import streamlit as st
-from data import get_data
-
-st.title("Robot Saham Indonesia")
-
-symbol = st.text_input("Kode Saham", "BBCA.JK")
-
-timeframe = st.selectbox(
-    "Timeframe",
-    ["5m","15m","30m","60m","1d"]
-)
-
-df = get_data(symbol, timeframe)
-
-if df.empty:
-    st.warning("Data kosong")
-else:
-    st.line_chart(df['close'])
-    st.write(df.tail())
-import streamlit as st
 from data import get_data, add_indicators
 
 st.title("Robot Saham Indonesia")
 
-symbol = st.text_input("Kode Saham", "BBCA.JK")
+symbol = st.text_input("Kode Saham", "BBCA.JK", key="symbol_input")
 
 timeframe = st.selectbox(
     "Timeframe",
-    ["5m","15m","30m","60m","1d"]
+    ["5m","15m","30m","60m","1d"],
+    key="timeframe_select"
 )
 
 df = get_data(symbol, timeframe)
@@ -35,6 +17,5 @@ if df.empty:
     st.warning("Data kosong")
 else:
     df = add_indicators(df)
-
     st.line_chart(df[['close','ema20','ema50']])
     st.write(df.tail())
