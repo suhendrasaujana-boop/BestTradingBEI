4836d079956a8db1de7530736123d296892f3159
suhendrasaujana-boop <272155887+suhendrasaujana-boop@users.noreply.github.com>
2026-04-17 12:33:11 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 
 def get_data(symbol="BBCA.JK", interval="5m"):
     df = yf.download(
@@ -8,7 +9,12 @@ def get_data(symbol="BBCA.JK", interval="5m"):
         progress=False
     )
 
+    if df is None or df.empty:
+        return pd.DataFrame()
+
     df = df.reset_index()
-    df.columns = [c.lower() for c in df.columns]
+
+    # rename aman
+    df.columns = [str(c).lower() for c in df.columns]
 
     return df
