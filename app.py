46ffa8797a442acc6a5567ba6836d4cbbfbc555a
suhendrasaujana-boop <272155887+suhendrasaujana-boop@users.noreply.github.com>
2026-04-17 12:20:07 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from data import get_data
+
+st.title("Robot Saham Indonesia")
+
+symbol = st.text_input("Kode Saham", "BBCA.JK")
+
+timeframe = st.selectbox(
+    "Timeframe",
+    ["1m","5m","15m","30m","60m","1d"]
+)
+
+df = get_data(symbol, timeframe)
+
+st.line_chart(df['close'])
+st.write(df.tail())
